5da944227782c14f33b79e5c7d8fa29e637e56a3
Ryan Twohey <ryantwohey@Ryans-MBP-2.attlocal.net>
2022-10-28 15:01:42 -0400

    Implemented basic version of barcode scanning

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'io.ionic.react.barcode',
   appName: 'react-barcode',
   webDir: 'build',
   bundledWebRuntime: false
